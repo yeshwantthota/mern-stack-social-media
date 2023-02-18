c9fe13f23474ab05941e701a5fa66f1291c0c8f7
Yeshwant Thota <thotayeshwant@gmail.com>
2023-02-18 22:36:19 +0530

    authentication and authorization endpoints setup

--- server/index.js
+++ server/index.js
@@ -8,7 +8,9 @@ import path from "path";
 import { fileURLToPath } from "url";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import { register } from "./controllers/auth.js";
 /* CONFIGURAIONS */
 
 const __filename = fileURLToPath(import.meta.url);
@@ -39,6 +41,8 @@ const upload = multer({ storage });
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
 
+/* ROUTES */
+app.use("/auth", authRoutes);
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 
