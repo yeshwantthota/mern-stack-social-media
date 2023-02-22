2dec07891eeaef9ac43f7907596d4a50a9693917
Yeshwant Thota <thotayeshwant@gmail.com>
2023-02-22 12:32:58 +0530

    navbar done

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,5 +1,11 @@
+import Navbar from "scenes/navbar";
+import { Box } from "@mui/system";
 const HomePage = () => {
-  return <div>Homepage</div>;
+  return (
+    <Box>
+      <Navbar />
+    </Box>
+  );
 };
 
 export default HomePage;
